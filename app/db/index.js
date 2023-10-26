c380a9dc0ac3835c7633569c4187bcf07a77e4ee
Huy0110 <duchuy01102001@gmail.com>
2023-10-26 04:38:43 +0000

    Refactor code

---
+++ app/db/index.js
@@ -0,0 +1,56 @@
+const db = require("../models");
+const dbConfig = require('../config/db.config')
+const Role = db.role;
+
+console.log(`mongodb://${dbConfig.USERNAME}:${dbConfig.PASSWORD}@${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`)
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "Agritech",
+    user: dbConfig.USERNAME,
+    pass: dbConfig.PASSWORD
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+function initial() {
+  Role.estimatedDocumentCount()
+    .then(count => {
+      if (count === 0) {
+        const userRole = new Role({
+          name: "user"
+        });
+
+        const moderatorRole = new Role({
+          name: "moderator"
+        });
+
+        const adminRole = new Role({
+          name: "admin"
+        });
+
+        return Promise.all([userRole.save(), moderatorRole.save(), adminRole.save()]);
+      }
+    })
+    .then(savedRoles => {
+      if (savedRoles) {
+        savedRoles.forEach(savedRole => {
+          console.log(`added '${savedRole.name}' to roles collection`);
+        });
+      }
+    })
+    .catch(err => {
+      console.error("Error when counting documents or saving roles", err);
+    });
+}
+
+module.exports = {
+  db
+};
--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const dbConfig = require('./app/config/db.config')
+const db = require("./app/db")
 
 const app = express();
-
-var corsOptions = {
-  origin: "*"
-};
-
 app.use(cors());
 
 // parse requests of content-type - application/json
@@ -20,59 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to huy application." });
 });
-
-const db = require("./app/models");
-const Role = db.role;
-
-console.log(`mongodb://${dbConfig.USERNAME}:${dbConfig.PASSWORD}@${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`)
-db.mongoose
-  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    dbName: "Agritech",
-    user: dbConfig.USERNAME,
-    pass: dbConfig.PASSWORD
-  })
-  .then(() => {
-    console.log("Successfully connect to MongoDB.");
-    initial();
-  })
-  .catch(err => {
-    console.error("Connection error", err);
-    process.exit();
-  });
-
-  function initial() {
-    Role.estimatedDocumentCount()
-      .then(count => {
-        if (count === 0) {
-          const userRole = new Role({
-            name: "user"
-          });
-
-          const moderatorRole = new Role({
-            name: "moderator"
-          });
-
-          const adminRole = new Role({
-            name: "admin"
-          });
-
-          return Promise.all([userRole.save(), moderatorRole.save(), adminRole.save()]);
-        }
-      })
-      .then(savedRoles => {
-        if (savedRoles) {
-          savedRoles.forEach(savedRole => {
-            console.log(`added '${savedRole.name}' to roles collection`);
-          });
-        }
-      })
-      .catch(err => {
-        console.error("Error when counting documents or saving roles", err);
-      });
-  }
-
 
 // routes
 require('./app/routes/auth.routes')(app);
